37c5c294f3739333668b938932eeee733e43d703
vButkevich <vyacheslav.butkevich@gmail.com>
2024-07-20 15:59:41 +0300

    fix:path.2

--- src/constants/contacts.js
+++ src/constants/contacts.js
@@ -1,4 +1,5 @@
-// export const PATH_DB = './src/db/db.json';
 import path from 'node:path';
-export const PATH_DB = path.join('src', 'db', 'db.json');
-// console.log('PATH_DB:', PATH_DB);
+export const PATH_DB = path.join(process.cwd(),'src', 'db', 'db.json');
+// export const PATH_DB = path.resolve('db.json');
+// export const PATH_DB = './src/db/db.json';
+// console.log({PATH_DB});
